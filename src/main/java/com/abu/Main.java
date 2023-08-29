fdb99fd21b85668b096967259990f3cc44e4f240
Mohamed Abdullah <abdullahfakrudeen2020@gmail.com>
2023-08-29 22:56:52 +0530

    added some comments

--- src/main/java/com/abu/Main.java
+++ src/main/java/com/abu/Main.java
@@ -6,7 +6,9 @@ import javax.security.auth.login.LoginException;
 
 public class Main  {
     public static void main(String[] args) throws LoginException {
-
+    	
+    	
+    	// creates an object for yelan bot class
         yelanBot yelan = new yelanBot();
     }
 }
--- src/main/java/com/abu/bot/events/EventListener.java
+++ src/main/java/com/abu/bot/events/EventListener.java
@@ -9,14 +9,19 @@ public class EventListener extends ListenerAdapter {
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
 
+    	// get the user who reacts to the messages
         User username = event.getUser();
 
+        // get the reacted emoji
         String emoji = event.getReaction().getEmoji().getAsReactionCode();
 
+        // get the channel the user reacted
         String channelMention = event.getChannel().getAsMention();
 
+        // this message was displayed
         String message = username.getAsMention()+" reacted with this "+emoji+" emoji in "+channelMention+" Channel";
 
+        
         event.getGuild().getTextChannelById("1146097570112884838").sendMessage(message).queue();
 
 
