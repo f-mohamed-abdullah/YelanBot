7d16bd3bb91f730b7d3cc72d67269701f993a131
abdullah <abdullahfakrudeen2020@gmail.com>
2023-09-11 10:48:41 +0530

    added hug the user command

---
+++ src/main/java/com/abu/bot/events/Greetings.java
@@ -0,0 +1,8 @@
+package com.abu.bot.events;
+
+import net.dv8tion.jda.api.events.message.GenericMessageEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Greetings extends ListenerAdapter {
+
+}
--- src/main/java/com/abu/bot/yelanBot.java
+++ src/main/java/com/abu/bot/yelanBot.java
@@ -1,7 +1,8 @@
 package com.abu.bot;
 
-import com.abu.bot.commands.commandManager;
+import com.abu.bot.commands.CommandManager;
 import com.abu.bot.events.ChannelCreateandDelete;
+import com.abu.bot.events.Greetings;
 import com.abu.bot.events.GuildJoinandLeave;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
@@ -19,7 +20,7 @@ public class yelanBot {
         JDABuilder builder = JDABuilder.createDefault(token);
 
         builder.enableIntents(GatewayIntent.GUILD_MEMBERS,GatewayIntent.GUILD_MESSAGES,GatewayIntent.GUILD_PRESENCES);
-        builder.addEventListeners(new ChannelCreateandDelete(),new commandManager(),new GuildJoinandLeave());
+        builder.addEventListeners(new ChannelCreateandDelete(),new CommandManager(),new GuildJoinandLeave(),new Greetings());
 
         botManager = builder.build();
 
