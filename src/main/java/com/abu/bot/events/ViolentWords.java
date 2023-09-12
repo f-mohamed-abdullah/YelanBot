6adf6456ba81a8cfa42327c82156077044700393
Mohamed Abdullah <abdullahfakrudeen2020@gmail.com>
2023-09-12 12:00:07 +0530

    added violent words warning

--- src/main/java/com/abu/bot/events/ViolentWords.java
+++ src/main/java/com/abu/bot/events/ViolentWords.java
@@ -6,26 +6,30 @@ import net.dv8tion.jda.api.utils.FileUpload;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ViolentWords extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
-        String[] dontusethesewords = {"fuck","bitch","bastard","dick","pussy","boobs","motherfucker","asshole"};
+        String[] dontusethesewords = {"fuck","bitch","bastard","dick","pussy","boobs","motherfucker","asshole","nigga","ass",
+                                        "cum","slut","whore"};
 
         ArrayList<String> angrypicspath = new ArrayList<>();
         angrypicspath.add("images/angry/angry-1.jpeg");
         angrypicspath.add("images/angry/angry-2.jpeg");
-        angrypicspath.add("images/angry/angry-3.jpeg");
-        angrypicspath.add("images/angry/angry-4.jpeg");
-        angrypicspath.add("images/angry/angry-5.jpeg");
+        angrypicspath.add("images/angry/angry-3.jpg");
+        angrypicspath.add("images/angry/angry-4.jpg");
+        angrypicspath.add("images/angry/angry-5.jpg");
 
         String message = event.getMessage().getContentRaw().toLowerCase();
 
         for (String i:dontusethesewords){
             if (message.contains(i)){
-                File f = new File("images/angry/angry-2.jpeg");
+                int index = new Random().nextInt(angrypicspath.size());
+                String path = angrypicspath.get(index);
+                File f = new File(path);
                 event.getGuild().getSystemChannel().sendFiles(FileUpload.fromData(f)).queue();
                 event.getGuild().getSystemChannel().sendMessage(event.getMember().getAsMention()+" Don't use violent words in server!!!").queue();
             }
