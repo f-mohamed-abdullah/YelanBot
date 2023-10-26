8f2b1e3507ae0a46c68f83351f4030d7f889ff7a
Mohamed Abdullah <abdullahfakrudeen2020@gmail.com>
2023-10-26 21:39:13 +0530

    added good morning and good night greetings function

--- src/main/java/com/abu/Main.java
+++ src/main/java/com/abu/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) throws LoginException {
 
 
-        // creates an object for yelan bot class
+
         yelanBot yelan = new yelanBot();
 
     }
--- src/main/java/com/abu/bot/events/Greetings.java
+++ src/main/java/com/abu/bot/events/Greetings.java
@@ -1,9 +1,34 @@
 package com.abu.bot.events;
 
 import net.dv8tion.jda.api.events.message.GenericMessageEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Greetings extends ListenerAdapter {
 
+    @Override
+    public void onReady(ReadyEvent event) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
 
+            @Override
+            public void run() {
+                LocalTime now = LocalTime.now();
+                LocalTime MorningTime = LocalTime.of(6,1);
+                LocalTime NightTime = LocalTime.of(21, 1);
+                if(now.isAfter(NightTime)){
+                    event.getJDA().getTextChannelById("1150429037370556494").sendMessage("Good Night 💤").queue();
+                }
+            }
+
+
+        },0,60000);
+
+
+
+    }
 }
