83507bb6da707d012b5ae731fa6a5d4ce3d6c595
mdxabu <abdullahfakrudeen2020@gmail.com>
2023-11-26 19:07:25 +0530

    Created Some embedded message

--- src/main/java/com/abu/Bot/events/Information.java
+++ src/main/java/com/abu/Bot/events/Information.java
@@ -1,16 +1,28 @@
 package com.abu.Bot.events;
 
+import com.abu.Bot.Embeds.EmbeddedMessageBuilder;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Information extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        EmbeddedMessageBuilder embed = new EmbeddedMessageBuilder();
         String messageRecievedChannel = event.getMessage().getChannelId();
-        if(event.getMessage().getContentRaw().equals("!info")){
-            event.getGuild().getTextChannelById(messageRecievedChannel).sendMessage("- ***Yelan*** is an Multipurpose discord bot 🤖.\n" +
-                    "- Inspired from genshin impact pc game 🎮 character.").queue();
+
+        if(event.getMessage().getContentRaw().equals("!info"))
+        {
+
+//           embed.
+
+            event.getGuild().getTextChannelById(messageRecievedChannel).sendMessage("").setEmbeds(embed.build()).queue();
         }
+
     }
 }
