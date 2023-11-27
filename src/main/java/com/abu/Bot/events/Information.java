ce0468226b01635a299478edc56b997d873d7ee0
mdxabu <abdullahfakrudeen2020@gmail.com>
2023-11-27 11:51:08 +0530

    added thumbnail to the embedded message

--- src/main/java/com/abu/Bot/events/Information.java
+++ src/main/java/com/abu/Bot/events/Information.java
@@ -5,6 +5,8 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Information extends ListenerAdapter {
 
@@ -19,7 +21,7 @@ public class Information extends ListenerAdapter {
             embed.setColor(new Color(76, 0, 130));
             embed.setDescription("- ***Yelan*** is an Multipurpose discord bot 🤖\n"+
                     "- Inspired from genshin impact pc game 🎮 character.");
-
+            embed.setThumbnail("https://image.tensorartassets.com/posts/images/612675178081572992/0604a0db-1835-4de3-b769-360840a0d991.jpg");
             embed.setFooter("See you again...");
 
             event.getGuild().getTextChannelById(messageRecievedChannel).sendMessage("").setEmbeds(embed.build()).queue();
