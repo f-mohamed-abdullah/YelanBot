1d8466d7c52798eae470c0cfaf7496da73be917a
Mohamed Abdullah <abdullahfakrudeen2020@gmail.com>
2024-04-27 11:54:16 +0530

    Added music support

--- src/main/java/com/abu/Bot/commands/CommandManager.java
+++ src/main/java/com/abu/Bot/commands/CommandManager.java
@@ -1,142 +1,42 @@
 package com.abu.Bot.commands;
 
-import com.abu.Bot.Embeds.EmbeddedMessageBuilder;
-import com.eme22.imageapi.AnimeImageClient;
-import com.eme22.imageapi.util.Endpoints;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.utils.FileUpload;
+import org.jetbrains.annotations.NotNull;
 
-import javax.print.DocFlavor;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class CommandManager extends ListenerAdapter {
 
-
+    private List<ICommand> commands = new ArrayList<>();
 
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        String command = event.getName();
-
-        AnimeImageClient client = new AnimeImageClient();
-
-        switch (command) {
-
-            // This command show all roles available in server
-            case "showallroles" -> {
-                event.deferReply().queue();
-                StringBuilder response = new StringBuilder();
-                for (Role role : event.getGuild().getRoles()) {
-                    response.append(role.getAsMention()).append("\n");
+    public void onReady(@NotNull ReadyEvent event) {
+        for(Guild guild : event.getJDA().getGuilds()) {
+            for(ICommand command : commands) {
+                if(command.getOptions() == null) {
+                    guild.upsertCommand(command.getName(), command.getDescription()).queue();
+                } else {
+                    guild.upsertCommand(command.getName(), command.getDescription()).addOptions(command.getOptions()).queue();
                 }
-                event.getHook().sendMessage(response.toString()).queue();
             }
-            case "ily" -> {
-                ArrayList<String> cutePicsofYelan = new ArrayList<>();
-                cutePicsofYelan.add("images/ily/cute1.jpg");
-                cutePicsofYelan.add("images/ily/cute2.jpg");
-                cutePicsofYelan.add("images/ily/cute3.jpg");
-                cutePicsofYelan.add("images/ily/cute4.jpg");
-                cutePicsofYelan.add("images/ily/cute5.jpg");
-                cutePicsofYelan.add("images/ily/cute6.jpg");
-                cutePicsofYelan.add("images/ily/cute7.jpg");
-
-                String username = event.getUser().getAsMention();
-                int index = new Random().nextInt(cutePicsofYelan.size());
-                String path = cutePicsofYelan.get(index);
-                File f = new File(path);
-
-                event.replyFiles(FileUpload.fromData(f)).queue();
-                event.getHook().sendMessage("I Love You too " + username + " </3 !!!").queue();
-            }
-            case "hiii_yelan" -> {
-                String username = event.getUser().getAsMention();
-                event.reply("Hello "+username+" ❤️❤️❤️").queue();
-            }
-            case "hug" -> {
-//                ArrayList<String> hugPicsofgenshin = new ArrayList<>();
-
-//                hugPicsofgenshin.add("images/hug/hug-1.gif");
-//                hugPicsofgenshin.add("images/hug/hug-2.jpeg");
-//                hugPicsofgenshin.add("images/hug/hug-3.jpeg");
-//                hugPicsofgenshin.add("images/hug/hug-4.jpeg");
-//                hugPicsofgenshin.add("images/hug/hug-5.jpeg");
-//                hugPicsofgenshin.add("images/hug/hug-6.jpg");
-//                hugPicsofgenshin.add("images/hug/hug-7.jpg");
-//                hugPicsofgenshin.add("images/hug/hug-8.gif");
-//                hugPicsofgenshin.add("images/hug/hug-9.gif");
-                EmbedBuilder hugEmbed =EmbeddedMessageBuilder.getEmbeds();
-
-
-
-                OptionMapping hugOption = event.getOption("user");
-
-                String huggedBy = event.getUser().getAsMention();
-
-                assert hugOption != null;
-                String huggedUser = hugOption.getAsUser().getAsMention();
-
-                String message = huggedUser+" was hugged by "+huggedBy+" :)";
-                String hugUrl = client.getWaifuEndPoint(Endpoints.WAIFU_SFW.HUG);
-
-                hugEmbed.setImage(hugUrl);
-                event.reply(message).setEmbeds(hugEmbed.build()).queue();
-
-            }
-            case "avatar" ->{
-                EmbedBuilder avatarEmbed = EmbeddedMessageBuilder.getEmbeds();
-                OptionMapping avatarOption = event.getOption("user");
-                User us = avatarOption.getAsUser();
-
-                String avatarurl = us.getEffectiveAvatarUrl();
-
-
-                String avatarUser = avatarOption.getAsUser().getAsMention();
-                avatarEmbed.setImage(avatarurl);
-                event.reply("").setEmbeds(avatarEmbed.build()).queue();
-            }
-            case "ping" ->{
-                event.reply("Pong!!!").queue();
-            }
-
-            default -> event.reply("OOPS!!!").queue();
         }
     }
 
-
-
     @Override
-    public void onGuildReady(GuildReadyEvent event) {
-        List<CommandData> commandData = new ArrayList<>();
-
-
-        commandData.add(Commands.slash("ily","Say I Love You </3 to yelan"));
-        commandData.add(Commands.slash("showallroles","See all roles on server"));
-        commandData.add(Commands.slash("hiii_yelan","say hi to yelan"));
-        commandData.add(Commands.slash("ping","You say ping and i say pong"));
-
-//        OptionData option1 = new OptionData(OptionType.USER,"username","hug the user you want",false);
-        OptionData option2 = new OptionData(OptionType.MENTIONABLE,"user","hugging");
-        OptionData option3 = new OptionData(OptionType.MENTIONABLE,"user","Avatar");
-
-        commandData.add(Commands.slash("hug","hug the user you want !!!").addOptions(option2));
-        commandData.add(Commands.slash("avatar","Avatar of the user you want to see!!!").addOptions(option3));
-        event.getGuild().updateCommands().addCommands(commandData).queue();
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        for(ICommand command : commands) {
+            if(command.getName().equals(event.getName())) {
+                command.execute(event);
+                return;
+            }
+        }
     }
 
-
+    public void add(ICommand command) {
+        commands.add(command);
+    }
 }
--- src/main/java/com/abu/Bot/yelanBot.java
+++ src/main/java/com/abu/Bot/yelanBot.java
@@ -2,7 +2,8 @@ package com.abu.Bot;
 
 
 import com.abu.Bot.Embeds.EmbeddedMessageBuilder;
-import com.abu.Bot.commands.CommandManager;
+import com.abu.Bot.Lavaplayer.AudioForwarder;
+import com.abu.Bot.commands.*;
 import com.abu.Bot.events.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -23,14 +24,15 @@ public class yelanBot {
         builder.enableIntents(GatewayIntent.GUILD_MEMBERS,GatewayIntent.GUILD_MESSAGES,GatewayIntent.MESSAGE_CONTENT,GatewayIntent.GUILD_PRESENCES);
         builder.addEventListeners(new ChannelCreateandDelete());
         builder.addEventListeners(new EmojiEvents());
+//        builder.addEventListeners(new Play());
 //        builder.addEventListeners(new RoleCreateandDelete());
         builder.addEventListeners(new ViolentWords());
         builder.addEventListeners(new Greetings());
-        builder.addEventListeners(new CommandManager());
+//        builder.addEventListeners(new CommandManager());
         builder.addEventListeners(new GuildJoinandLeave());
         builder.addEventListeners(new Information());
 
-        builder.addEventListeners(new music());
+//        builder.addEventListeners(new music());
         builder.addEventListeners(new developer());
         builder.addEventListeners(new EmbeddedMessageBuilder());
         builder.addEventListeners(new AnonymousMessageSender());
@@ -38,10 +40,21 @@ public class yelanBot {
         builder.addEventListeners(new EmbeddedMessageBuilder());
 
 
+        CommandManager manager = new CommandManager();
+        
+        manager.add(new Play());
+        manager.add(new Skip());
+        manager.add(new Stop());
+        manager.add(new NowPlaying());
+        manager.add(new Queue());
+        manager.add(new Repeat());
+        builder.addEventListeners(manager);
+
 //        builder.setActivity(Activity.listening("Shadow in the Rain"));
         builder.setActivity(Activity.playing("Genshin Impact"));
 
         botManager = builder.build();
+
 
 
 
